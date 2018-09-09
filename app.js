1faad937be7771ee62126fd2fd75e6c42f965bac
Peter Kim <pkim@boardeffect.com>
2018-09-09 18:40:02 -0400

    Reference folders correctly

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('./api/public'));
 
-app.use('/api', indexRouter);
-app.use('/api/users', usersRouter);
+app.use('./api', indexRouter);
+app.use('./api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
