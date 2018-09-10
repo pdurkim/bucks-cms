48ddcf4ac632b3ec901dfd2b4123533c8a115c08
Peter Kim <pkim@boardeffect.com>
2018-09-09 20:34:47 -0400

    Use client/build if production environment.

--- app.js
+++ app.js
@@ -10,14 +10,20 @@ var usersRouter = require('./api/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files from the React app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+} else {
+  app.use(express.static(path.join(__dirname, 'client/public')));
+}
 
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
