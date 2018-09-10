1d67afc4b8e6a72d6dd29c43d4713654f72ad260
Peter Kim <pkim@boardeffect.com>
2018-09-09 20:23:24 -0400

    Revert app.js

--- app.js
+++ app.js
@@ -10,22 +10,17 @@ var usersRouter = require('./api/routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', 'api/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('./client/build'));
-}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('./api/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('./api', indexRouter);
-app.use('./api/users', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
